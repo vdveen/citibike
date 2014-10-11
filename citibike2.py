8ce429e40362def821c62952dff2abe267c3c6ec
vdveen <asvdveen@Annes-MacBook-Pro.local>
2014-10-10 23:18:15 -0600

    Converter script done!
    Citibike2.py now successfully converts raw data to data I can work
    with! Whoo.

--- citibike2.py
+++ citibike2.py
@@ -1,13 +1,15 @@
 #Code written by vdveen
 #Purpose: TBD yo
 
-opendata = open('Citibike10.csv')
+opendata = open('Citibike10.csv')  #Open and read file
 rawdata = opendata.read()
-rawdata = rawdata.replace('\r', ',')
-rawdata = rawdata.split(',')
+rawdata = rawdata.replace('\r', ',')  #Replace newlines with commas
+rawdata = rawdata.split(',')       #Then split on commas
 
+#New list to add to
 bikedata = []
 
+#Check all items in data, remove brackets, convert it into a flawless file.
 for item in rawdata:
   if item == '""tripduration':
     item = 'tripduration'
@@ -17,19 +19,23 @@ for item in rawdata:
     item = 'starttime'
     bikedata.append(item)
 
+  elif item.startswith('""') and item.endswith('"""'):
+      item = item[2:-3]
+      bikedata.append(item)
+
   elif item.startswith('""') and item.endswith('""'):
     item = item[2:-2]
     bikedata.append(item)
+
 
   elif item.startswith('"'):
     item = item[1:]
     item = '\r' + item
     bikedata.append(item)
 
+print List converted
 
-print bikedata
-
-
+#Add neatly converted files to output file
 newfile = open('Citibike10-2.csv', 'w')
 for item in bikedata:
   newfile.write(item)
