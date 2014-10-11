15c7795e043f6162b0c01f9a2d194a36794cc503
vdveen <asvdveen@Annes-MacBook-Pro.local>
2014-10-10 23:05:27 -0600

    Got it almost successful
    problem, it only does half of the lines correctly

--- citibike.py
+++ citibike.py
@@ -1,12 +1,17 @@
-#Code written by vdveen
 #Purpose: TBD yo
 
 opendata = open('Citibike10.csv')
+bikedata = opendata.read()
+test = bikedata.split('\"\"')
+for item in test:
+  if item == ',':
+    test.remove(',')
 rawdata = opendata.read()
-rawdata = rawdata.split('\r')
-print len(rawdata)
-print rawdata
-bikedata = rawdata
+rawdata = rawdata.split(',')
+#fuck I am missing the first and last column, gender and tripduration
+
+print type(test)
+bikedata = []
 
 for item in rawdata:
   if item == '""tripduration':
@@ -21,15 +26,23 @@ for item in rawdata:
     item = item[2:-2]
     if item.endswith('year') or len(item) == 1:
       item = item + '\r'
-    bikedata.append(item)
+      bikedata.append(item)
+    else:
+      bikedata.append(item)
 
 
 print bikedata
 
+test = bikedata.split(',')
+print test
 
 newfile = open('Citibike10-2.csv', 'w')
+for item in test:
+  newfile.write(str(test))
 for item in bikedata:
   newfile.write(item)
   newfile.write(',')
-
-newfile.close()
+  if '\r' in item:
+    newfile.write('\r')
+    print item
+ newfile.close()
--- citibike2.py
+++ citibike2.py
@@ -3,10 +3,10 @@
 
 opendata = open('Citibike10.csv')
 rawdata = opendata.read()
-rawdata = rawdata.split('\r')
-print len(rawdata)
-print rawdata
-bikedata = rawdata
+rawdata = rawdata.replace('\r', ',')
+rawdata = rawdata.split(',')
+
+bikedata = []
 
 for item in rawdata:
   if item == '""tripduration':
@@ -19,8 +19,11 @@ for item in rawdata:
 
   elif item.startswith('""') and item.endswith('""'):
     item = item[2:-2]
-    if item.endswith('year') or len(item) == 1:
-      item = item + '\r'
+    bikedata.append(item)
+
+  elif item.startswith('"'):
+    item = item[1:]
+    item = '\r' + item
     bikedata.append(item)
 
 
@@ -31,5 +34,4 @@ newfile = open('Citibike10-2.csv', 'w')
 for item in bikedata:
   newfile.write(item)
   newfile.write(',')
-
 newfile.close()
