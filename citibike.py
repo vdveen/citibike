b1b93b65b1eb50100af0ac817cbede0adc3bcb2b
vdveen <asvdveen@Annes-MacBook-Pro.local>
2014-10-10 21:52:25 -0600

    Split test

--- citibike.py
+++ citibike.py
@@ -3,5 +3,17 @@
 
 opendata = open('Citibike10.csv')
 bikedata = opendata.read()
-print bikedata
+test = bikedata.split('\"\"')
+for item in test:
+  if item == ',':
+    test.remove(',')
 
+print type(test)
+
+test = bikedata.split(',')
+print test
+
+newfile = open('Citibike10-2.csv', 'w')
+for item in test:
+  newfile.write(str(test))
+newfile.close()
