2c969807841d54bc0518e14475fb3477d0dfe11f
vdveen <asvdveen@Annes-MacBook-Pro.local>
2014-10-17 17:07:19 -0600

    Tested it on full file
    Mixed results…

--- patternfinder.py
+++ patternfinder.py
@@ -10,7 +10,7 @@ gender = []
 
 for item in line:         #Getting the relevant values
   count = count + 1
-  if count <= 500000:
+  if count <= 8000000:
     if count % 9 == 1 or count % 9 == 0:   #Gender or tripduration
       if 'd' in item:               #Making sure header is ignored
         item = item
@@ -21,23 +21,21 @@ for item in line:         #Getting the relevant values
         trip.append(int(item))
 
 #Now that gender and trip list are filled, this function zips them together
-dictionary = dict(zip(trip, gender))
-
-print dictionary
+dictionary = zip(trip, gender)
 
 #Now lets separate the male and female values
 #and put them in a new list
 maletripduration = []
 femaletripduration = []
 
-for value in dictionary:
-    if dictionary[value] == 1:
-      maletripduration.append(int(value))
 
 for value in dictionary:
-    if dictionary[value] == 2:
-      femaletripduration.append(int(value))
+    if value[1] == 1:
+      maletripduration.append(int(value[0]))
 
+for value in dictionary:
+    if value[1] == 2:
+      femaletripduration.append(int(value[0]))
 
 print 'The Average for males is:', sum(maletripduration) / len(maletripduration)
 print 'The Average for females is:', sum(femaletripduration) / len(femaletripduration)
